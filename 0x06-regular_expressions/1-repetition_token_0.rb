3b20aa2b4933603326b9cc1be0779adf4dfe1c3b
Essy <esthercikechukwu@gmail.com>
2024-01-04 07:31:27 +0000

    task1

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,20 +1,2 @@
 #!/usr/bin/env ruby
-
-def match_school(input)
-regex = /\bSchool\b/i
-
-  if input =~ regex
-    puts "The input '#{input}' contains the word 'School'."
-  else
-    puts "The input '#{input}' does not contain the word 'School'."
-  end
-end
-
-if ARGV.empty?
-  puts "Please provide a string as a command-line argument."
-else
-
-  input_string = ARGV[0]
-
-  match_school(input_string)
-end
+puts ARGV[0].scan(/hbt{2,5}n/).join
