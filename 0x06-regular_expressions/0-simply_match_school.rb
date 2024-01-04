821e50f32c76394fcc4d462390d5506ce0f28165
Essy <esthercikechukwu@gmail.com>
2024-01-04 06:51:38 +0000

    tassk 0

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,25 +1,2 @@
 #!/usr/bin/env ruby
-
-# filename: match_school.rb
-
-def match_school(input)
-
-  regex = /\bSchool\b/
-
-  if input =~ regex
-    puts "The input '#{input}' matches the pattern 'School'."
-  else
-    puts "The input '#{input}' does not match the pattern 'School'."
-  end
-end
-
-
-if ARGV.empty?
-  puts "Please provide a string as a command-line argument."
-else
-
-  input_string = ARGV[0]
-
-
-  match_school(input_string)
-end
+puts ARGV[0].scan(/Schoo/)/join
